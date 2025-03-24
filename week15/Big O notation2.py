19259ae4519a749a5411f5b8bd02ea3e3ec56f5c
Roberto Vega <robjvc89@hotmail.com>
2025-03-24 13:25:46 -0600

    Added week15

--- week15/Big O notation2.py
+++ week15/Big O notation2.py
@@ -5,8 +5,8 @@ def print_numbers_times_2(numbers_list):
 
 
 def check_if_lists_have_an_equal(list_a, list_b):
-	for element_a in list_a:              #O(n) where n is the number of elements in the list.
-		for element_b in list_b:          #O(n) where n is the number of elements in the list.
+	for element_a in list_a:              #O(n²) due to the nested loops over the two lists
+		for element_b in list_b:          #O(n²) due to the nested loops over the two lists
 			if element_a == element_b:    #O(1) because comparing two elements is done in constant time.
 				return True               #O(1) it may terminate early when a match is found.
 				
@@ -23,9 +23,9 @@ def print_10_or_less_elements(list_to_print):
 
 def generate_list_trios(list_a, list_b, list_c):
 	result_list = []                       #O(1) This is constant time operation.
-	for element_a in list_a:               #O(n) where n is the number of elements in the list.
-		for element_b in list_b:           #O(n) where n is the number of elements in the list.
-			for element_c in list_c:       #O(n) where n is the number of elements in the list.
+	for element_a in list_a:               #O(a × b × c), where a, b, and c are the lengths of list_a, list_b, and list_c, respectively.
+		for element_b in list_b:           #O(a × b × c), where a, b, and c are the lengths of list_a, list_b, and list_c, respectively.
+			for element_c in list_c:       #O(a × b × c), where a, b, and c are the lengths of list_a, list_b, and list_c, respectively.
 				result_list.append(f'{element_a} {element_b} {element_c}')  #O(1), as appending to a list is a constant-time operation.
 				
 	return result_list                     #O(1) This is constant time operation.
